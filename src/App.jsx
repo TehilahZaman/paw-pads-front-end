f21399477c935b33c0cc4d227f01ff04b003de3e
Leah Irwin <irwin.leah@outlook.com>
2025-02-20 17:37:08 -0500

    added RentalList jsx/css and rough draft of RentalDetail.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 // src/App.jsx
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Routes, Route } from 'react-router'
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Dashboard from './components/Dashboard/Dashboard';
 import Landing from './components/Landing/Landing';
+import RentalList from './components/RentalList/RentalList';
+import RentalDetail from './components/RentalDetail/RentalDetail';
 
 import { UserContext } from './contexts/UserContext';
 const App = () => {
+  
+  const [rentals, setRentals] = useState([]);
  
   const { user } = useContext(UserContext)
 
@@ -19,6 +23,7 @@ const App = () => {
         <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/rentals' element={<RentalList rentals={rentals} />}/>
       </Routes> 
     </>
   );
