a71f99b539fdb6cd1d2d404275779a45dbd0d327
Tehilah Zamanzadeh <tehilah.zaman@gmail.com>
2025-02-21 09:56:40 -0800

    create a review

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 // src/App.jsx
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router'
-import NavBar from './components/NavBar/NavBar';
-import SignUpForm from './components/SignUpForm/SignUpForm';
-import SignInForm from './components/SignInForm/SignInForm';
-import Dashboard from './components/Dashboard/Dashboard';
-import Landing from './components/Landing/Landing';
+import { useContext } from "react";
+import { Routes, Route } from "react-router";
+import NavBar from "./components/NavBar/NavBar";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from "./components/SignInForm/SignInForm";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from "./components/Landing/Landing";
 
-import { UserContext } from './contexts/UserContext';
+import ReviewForm from "./components/ReviewForm/ReviewForm";
+
+import { UserContext } from "./contexts/UserContext";
 const App = () => {
- 
-  const { user } = useContext(UserContext)
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
-      </Routes> 
+        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/users/bookings/:bookingId" element={<ReviewForm />} />
+      </Routes>
     </>
   );
 };
@@ -27,4 +29,3 @@ const App = () => {
 export default App;
 
 // This is Zak's comment
-
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,38 +4,49 @@ import { Link } from "react-router";
 
 import { UserContext } from "../../contexts/UserContext";
 
-
 const NavBar = () => {
-    // Pass the UserContext object to the useContext hook to access:
+  // Pass the UserContext object to the useContext hook to access:
   // - The user state (which we use here).
   // - The setUser function to update the user state (which we aren't using).
   //
   // Destructure the object returned by the useContext hook for easy access
   // to the data we added to the context with familiar names.
-  const { user, setUser } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext);
 
-  function handleSignOut(){
-    // destroy the token! 
-    localStorage.removeItem('token')
+  function handleSignOut() {
+    // destroy the token!
+    localStorage.removeItem("token");
     // clearing out our state
-    setUser(null)
+    setUser(null);
   }
 
   return (
-<nav>
+    <nav>
       {user ? (
         <ul>
           <li>Welcome, {user.username}</li>
           {/* The new link */}
-          <li><Link to='/'>Dashboard</Link></li>
-          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
         </ul>
       ) : (
         <ul>
           {/* Another new link */}
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/sign-in'>Sign In</Link></li>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/sign-up">Sign Up</Link>
+          </li>
         </ul>
       )}
     </nav>
@@ -43,4 +54,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
