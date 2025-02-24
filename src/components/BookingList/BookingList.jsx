b06712d0d0a500ccb2cd59121704e3a114a1e47e
Zak Sandler <zaksandler@gmail.com>
2025-02-24 11:45:51 -0500

    attempting to get back to my branch

--- src/components/BookingList/BookingList.jsx
+++ src/components/BookingList/BookingList.jsx
@@ -10,26 +10,31 @@ const BookingList = () => {
   console.log(bookings);
   useEffect(() => {
     const fetchAllBookings = async () => {
+      try {
       console.log("the fetch function was called ");
       const bookingsData = await bookingService.index();
       console.log("bookingsData", bookingsData);
       setBookings(bookingsData);
+      } catch(err){
+          console.log(err.message, '<----error!')
+      }
     };
     fetchAllBookings();
   }, []);
 
+ 
   return (
     <main>
-      {!bookings.length ? <p>There are no bookings.</p> : null}
-      {/* added: if booking doesn't exist ... */}
-      {bookings.map((booking) => (
-        //  changed {} to ()
-        <Link key={booking._id} to={`/bookings/${booking._id}`}>
-          <article>
-            <header>
-              <h2>Booking for...</h2>
-              <p>
-                {`${booking.name} made a booking on
+        {!bookings.length ? <p>There are no bookings.</p> : null}
+        {/* added: if booking doesn't exist ... */}
+        {bookings.map((booking) => (
+            //  changed {} to ()
+            <Link key={booking._id} to={`/bookings/${booking._id}`}>
+                <article>
+                    <header>
+                        <h2>Booking for...</h2>
+                        <p>
+                            {`${booking.name} made a booking on
                                 ${new Date(
                                   booking.createdAt
                                 ).toLocaleDateString()}`}
